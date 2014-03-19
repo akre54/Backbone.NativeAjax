d80893cfb15ffc764b1c04e1e1d154c5052550c8
Adam Krebs <amk528@cs.nyu.edu>
2014-03-19 02:13:52 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+Backbone.NativeAjax
+===================
+
+A drop-in replacement for Backbone.Ajax that uses only native XMLHttpRequest
+methods for sync. It has no dependency on jQuery.
+
+To Use:
+-------
+Load Backbone.NativeAjax with your favorite module loader or add as a script
+tag after you have loaded Backbone in the page. You may set a `Backbone.Deferred`
+class that will be assigned to the return value if you so desire.
+
+Features:
+---------
+* Accepts `success` and `error` callbacks
+* Set headers with a `headers` object
+* `beforeSend`
+
+Requirements:
+-------------
+NativeAjax makes use of XMLHttpRequest which is supported in most modern browsers.
+See the [compatibility chart](http://caniuse.com/#search=XMLHttpRequest)
+
+Notes:
+------
+* The ajax function accepts a `success` and `error` callbacks. To return
+  a deferred object, set `Backbone.Deferred`.
+
+
+Uses code from [Exoskeleton](https://github.com/paulmillr/exoskeleton). See that
+project for more
+
---
+++ backbone.nativeajax.js
@@ -0,0 +1,117 @@
+// Backbone.NativeAjax.js 0.1.0
+// ---------------
+
+//     (c) 2014 Adam Krebs, Paul Miller, Exoskeleton Project
+//     Backbone.NativeAjax may be freely distributed under the MIT license.
+//     For all details and documentation:
+//     https://github.com/akre54/Backbone.NativeAjax
+
+(function (factory) {
+  if (typeof define === 'function' && define.amd) { define(['underscore', 'backbone'], factory);
+  } else if (typeof exports === 'object') { factory(require('underscore'), require('backbone'));
+  } else { factory(_, Backbone); }
+}(function (_, Backbone) {
+  // Make an AJAX request to the server.
+  // Usage:
+  //   var req = Backbone.ajax({url: 'url', type: 'PATCH', data: 'data'});
+  //   req.done(...).error(...) // if Backbone.Deferred is set
+  Backbone.ajax = (function() {
+    var xmlRe = /^(?:application|text)\/xml/;
+    var jsonRe = /^application\/json/;
+
+    var getData = function(accepts, xhr) {
+      if (accepts == null) accepts = xhr.getResponseHeader('content-type');
+      if (xmlRe.test(accepts)) {
+        return xhr.responseXML;
+      } else if (jsonRe.test(accepts)) {
+        return JSON.parse(xhr.responseText);
+      } else {
+        return xhr.responseText;
+      }
+    };
+
+    var isValid = function(xhr) {
+      return (xhr.status >= 200 && xhr.status < 300) ||
+        (xhr.status === 304) ||
+        (xhr.status === 0 && window.location.protocol === 'file:')
+    };
+
+    var end = function(xhr, options, deferred) {
+      return function() {
+        if (xhr.readyState !== 4) return;
+
+        var status = xhr.status;
+        var data = getData(options.headers && options.headers.Accept, xhr);
+
+        // Check for validity.
+        if (isValid(xhr)) {
+          if (options.success) options.success(data);
+          if (deferred) deferred.resolve(data);
+        } else {
+          var error = new Error('Server responded with a status of ' + status);
+          if (options.error) options.error(xhr, status, error);
+          if (deferred) deferred.reject(xhr);
+        }
+      }
+    };
+
+    return function(options) {
+      if (options == null) throw new Error('You must provide options');
+      if (options.type == null) options.type = 'GET';
+
+      var xhr = new XMLHttpRequest();
+      var deferred = Backbone.Deferred && Backbone.Deferred();
+
+      if (options.contentType) {
+        if (options.headers == null) options.headers = {};
+        options.headers['Content-Type'] = options.contentType;
+      }
+
+      // Stringify GET query params.
+      if (options.type === 'GET' && typeof options.data === 'object') {
+        var query = '';
+        var stringifyKeyValuePair = function(key, value) {
+          return value == null ? '' :
+            '&' + encodeURIComponent(key) +
+            '=' + encodeURIComponent(value);
+        };
+        for (var key in options.data) {
+          query += stringifyKeyValuePair(key, options.data[key]);
+        }
+
+        if (query) {
+          var sep = (options.url.indexOf('?') === -1) ? '?' : '&';
+          options.url += sep + query.substring(1);
+        }
+      }
+
+      if (options.credentials) options.withCredentials = true;
+      xhr.addEventListener('readystatechange', end(xhr, options, deferred));
+      xhr.open(options.type, options.url, true);
+
+      var allTypes = "*/".concat("*");
+      var xhrAccepts = {
+        "*": allTypes,
+        text: "text/plain",
+        html: "text/html",
+        xml: "application/xml, text/xml",
+        json: "application/json, text/javascript"
+      };
+      xhr.setRequestHeader(
+        "Accept",
+        options.dataType && xhrAccepts[options.dataType] ?
+          xhrAccepts[options.dataType] + (options.dataType !== "*" ? ", " + allTypes + "; q=0.01" : "" ) :
+          xhrAccepts["*"]
+      );
+
+      xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+      if (options.headers) for (var key in options.headers) {
+        xhr.setRequestHeader(key, options.headers[key]);
+      }
+      if (options.beforeSend) options.beforeSend(xhr);
+      xhr.send(options.data);
+
+      return deferred ? deferred.promise : undefined;
+    };
+  })();
+}));
