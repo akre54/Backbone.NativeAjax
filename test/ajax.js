83e44b44856101f96aed0a36508cb44ff73a97de
Adam Krebs <amk528@cs.nyu.edu>
2014-08-28 00:32:50 -0400

    remove backbone tests and stub out some basic specs

---
+++ test/ajax.js
@@ -0,0 +1,36 @@
+var expect = require('chai').expect,
+    sinon = require('sinon'),
+    NativeAjax = require('../');
+
+var xhr = sinon.useFakeXMLHttpRequest();
+
+describe('Backbone.NativeAjax', function() {
+  describe('setup', function() {
+    it('should export itself as Backbone.ajax');
+  });
+
+  describe('creating a request', function() {
+    it('should throw when no options object is passed');
+
+  });
+
+  describe('headers', function() {
+    it('should set headers if none passed in');
+    it('should use headers if passed in');
+  });
+
+  describe('finishing a request', function() {
+    it('should invoke the success callback on complete');
+    it('should invoke the error callback on error');
+  });
+
+  describe('Backbone.Deferred', function() {
+    it('should respect a Deferred property if one set', function() {
+
+    });
+
+    it('should resolve the deferred on complete');
+    it('should reject the deferred on error');
+
+  });
+});
